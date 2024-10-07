f0a720d43325b1e6d812694038067b5e7b1fe1d1
Maliktalha123 <talhanoormalik0321@gmail.com>
2024-10-08 00:42:48 +0500

    Header and Footer added...

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,148 +1,97 @@
+import { UserOutlined } from "@ant-design/icons";
+import { Input } from "antd";
 import React from "react";
 import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <footer className="text-gray-600 body-font">
-      <div className="container px-5 py-24 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
-        <div className="w-64 flex-shrink-0 md:mx-0 mx-auto text-center md:text-left">
-          <Link className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-          
-            <span className="ml-3 text-xl">Tailblocks</span>
-          </Link>
-          <p className="mt-2 text-sm text-gray-500">
-            Air plant banjo lyft occupy retro adaptogen indego
-          </p>
-        </div>
-        <div className="flex-grow flex flex-wrap md:pl-20 -mb-10 md:mt-0 mt-10 md:text-left text-center">
-          <div className="lg:w-1/4 md:w-1/2 w-full px-4">
-            <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">
-              CATEGORIES
-            </h2>
-            <nav className="list-none mb-10">
-              <li>
-                <Link className="text-gray-600 hover:text-gray-800">
-                  First Link
-                </Link>
-              </li>
-              <li>
-                <Link className="text-gray-600 hover:text-gray-800">
-                  Second Link
-                </Link>
-              </li>
-              <li>
-                <Link className="text-gray-600 hover:text-gray-800">
-                  Third Link
-                </Link>
-              </li>
-              <li>
-                <Link className="text-gray-600 hover:text-gray-800">
-                  Fourth Link
-                </Link>
-              </li>
-            </nav>
+    <footer className="text-gray-600 body-font borderTalha">
+      <div className="max-w-[1440px]  m-auto min-h-[425px] borderTalha">
+        <div className="container  px-5 py-12 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
+          <div className="w-64 flex-shrink-0 md:mx-0  text-start md:text-left">
+            <Link className="flex title-font font-medium   md:justify-start justify-start mb-12  text-gray-900">
+              <span className="ml-3 text-xl">Funiro.</span>
+            </Link>
+            <p className="mt-2 text-start text-sm text-gray-500">
+              400 University Drive Suite 200 Coral <br /> Gabies <br />
+              FL 33134 USA
+            </p>
           </div>
-          <div className="lg:w-1/4 md:w-1/2 w-full px-4">
-            <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">
-              CATEGORIES
-            </h2>
-            <nav className="list-none mb-10">
-              <li>
-                <Link className="text-gray-600 hover:text-gray-800">
-                  First Link
-                </Link>
-              </li>
-              <li>
-                <Link className="text-gray-600 hover:text-gray-800">
-                  Second Link
-                </Link>
-              </li>
-              <li>
-                <Link className="text-gray-600 hover:text-gray-800">
-                  Third Link
-                </Link>
-              </li>
-              <li>
-                <Link className="text-gray-600 hover:text-gray-800">
-                  Fourth Link
-                </Link>
-              </li>
-            </nav>
+          <div className="flex-grow flex flex-wrap md:pl-20 -mb-10 md:mt-0 mt-10 md:text-left text-center">
+            <div className="lg:w-1/4 md:w-1/2  w-full px-4">
+              <h2 className="title-font font-normal text-gray-900 tracking-widest text-sm mb-3">
+                CATEGORIES
+              </h2>
+              <nav className="list-none flex flex-col gap-8 mt-8 font-bold">
+                <li>
+                  <Link className="text-gray-600 hover:text-gray-800">
+                    Home
+                  </Link>
+                </li>
+                <li>
+                  <Link className="text-gray-600 hover:text-gray-800">
+                    Shop
+                  </Link>
+                </li>
+                <li>
+                  <Link className="text-gray-600 hover:text-gray-800">
+                    About
+                  </Link>
+                </li>
+                <li>
+                  <Link className="text-gray-600 hover:text-gray-800">
+                    Contact
+                  </Link>
+                </li>
+              </nav>
+            </div>
+            <div className="lg:w-1/4 md:w-1/2 w-full  px-4">
+              <h2 className="title-font font-normal text-gray-900 tracking-widest text-sm mb-3">
+                Help
+              </h2>
+              <nav className="list-none font-bold flex flex-col gap-8 mt-8">
+                <li>
+                  <Link className="text-gray-600 hover:text-gray-800">
+                    Payment Options
+                  </Link>
+                </li>
+                <li>
+                  <Link className="text-gray-600 hover:text-gray-800">
+                    Returns
+                  </Link>
+                </li>
+                <li>
+                  <Link className="text-gray-600 hover:text-gray-800">
+                  Privacy Policy
+                  </Link>
+                </li>
+                
+              </nav>
+            </div>
+            <div className="lg:w-2/4 md:w-1/2 w-full  px-4 ">
+              <h2 className="title-font font-normal text-gray-900 tracking-widest text-sm mb-3">
+Newsletter..
+              </h2>
+            <div className="flex gap-4">  <Input placeholder="Enter Your Email" prefix={<UserOutlined />} />  <span className="font-bold underline my-1 cursor-pointer"  >SUBSCRIBE</span></div>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="bg-gray-100">
-        <div className="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
-          <p className="text-gray-500 text-sm text-center sm:text-left">
-            © 2020 Tailblocks —
-            <Link
-              href="https://twitter.com/knyttneve"
-              rel="noopener noreferrer"
-              className="text-gray-600 ml-1"
-              target="_blank"
-            >
-              @knyttneve
-            </Link>
-          </p>
-          <span className="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
-            <Link className="text-gray-500">
-              <svg
-                fill="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                className="w-5 h-5"
-                viewBox="0 0 24 24"
+            <hr  />
+        
+          <div className="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
+            <p className=" text-sm text-center sm:text-left">
+               
+              <Link
+                to={'/'}
+                
+                className=" ml-1 font-extrabold"
+                
               >
-                <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-              </svg>
-            </Link>
-            <Link className="ml-3 text-gray-500">
-              <svg
-                fill="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                className="w-5 h-5"
-                viewBox="0 0 24 24"
-              >
-                <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-              </svg>
-            </Link>
-            <Link className="ml-3 text-gray-500">
-              <svg
-                fill="none"
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                className="w-5 h-5"
-                viewBox="0 0 24 24"
-              >
-                <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
-                <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
-              </svg>
-            </Link>
-            <Link className="ml-3 text-gray-500">
-              <svg
-                fill="currentColor"
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="0"
-                className="w-5 h-5"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  stroke="none"
-                  d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"
-                ></path>
-                <circle cx="4" cy="4" r="2" stroke="none"></circle>
-              </svg>
-            </Link>
-          </span>
+                2023 Furniro. All rights reserved
+              </Link>
+            </p>
+          </div>
         </div>
-      </div>
     </footer>
   );
 };
